57e893b6b9a347c7c08ec72bb4d77593516a3066
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-11-02 00:00:52 +0000

    サイドバーの形を作成

---
+++ resources/js/Shared/AppName/AppName.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AppName = () => {
+    return (
+        <div
+            id="AppName"
+            style={{
+                fontSize: "1rem",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "0.25rem",
+                fontFamily: "inherit",
+            }}
+        >
+            <h1>DisasterMealFinder</h1>
+        </div>
+    );
+};
+
+export default AppName;
---
+++ resources/js/Shared/Sidebar/Sidebar.tsx
@@ -0,0 +1,150 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+
+import "./css/Sidebar.css";
+import { Link } from "@inertiajs/react";
+import ApplicationLogo from "../../Components/ApplicationLogo";
+import AppName from "../AppName/AppName";
+
+const drawerWidth = 240;
+
+interface Props {
+    window?: () => Window;
+}
+
+export default function Sidebar(props: Props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+
+    const handleDrawerClose = () => {
+        setIsClosing(true);
+        setMobileOpen(false);
+    };
+
+    const handleDrawerTransitionEnd = () => {
+        setIsClosing(false);
+    };
+
+    const handleDrawerToggle = () => {
+        if (!isClosing) {
+            setMobileOpen(!mobileOpen);
+        }
+    };
+
+    const drawer = (
+        <div>
+            <Toolbar>
+                <Link href="/" className="sidebar-logo flex">
+                    <ApplicationLogo />
+                    <AppName />
+                </Link>
+            </Toolbar>
+
+            <Divider />
+            <List>
+                {["Inbox", "Starred", "Send email", "Drafts"].map(
+                    (text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? (
+                                        <InboxIcon />
+                                    ) : (
+                                        <MailIcon />
+                                    )}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    )
+                )}
+            </List>
+            <Divider />
+            <List>
+                {["All mail", "Trash", "Spam"].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const container =
+        window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <div id="Sidebar">
+            <Box sx={{ display: "flex" }}>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{ mr: 2, display: { md: "none" } }}
+                    className="hamburger-menu-button"
+                >
+                    <MenuIcon className="hamburger-menu-icon" />
+                </IconButton>
+                <Box
+                    component="nav"
+                    sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
+                    aria-label="mailbox folders"
+                >
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onTransitionEnd={handleDrawerTransitionEnd}
+                        onClose={handleDrawerClose}
+                        ModalProps={{
+                            keepMounted: true,
+                        }}
+                        sx={{
+                            display: { xs: "block", md: "none" },
+                            "& .MuiDrawer-paper": {
+                                boxSizing: "border-box",
+                                width: drawerWidth,
+                            },
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                    <Drawer
+                        variant="permanent"
+                        sx={{
+                            display: { xs: "none", md: "block" },
+                            "& .MuiDrawer-paper": {
+                                boxSizing: "border-box",
+                                width: drawerWidth,
+                            },
+                        }}
+                        open
+                    >
+                        {drawer}
+                    </Drawer>
+                </Box>
+            </Box>
+        </div>
+    );
+}
