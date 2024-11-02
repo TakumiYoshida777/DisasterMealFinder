d32fe393adf300959c25dd95c8c116a6e91d784c
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-11-02 04:22:34 +0000

    ユーザー情報のドロップダウンを実装

--- resources/js/Shared/Sidebar/Sidebar.tsx
+++ resources/js/Shared/Sidebar/Sidebar.tsx
@@ -16,9 +16,10 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 
 import "./css/Sidebar.css";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import ApplicationLogo from "../../Components/ApplicationLogo";
 import AppName from "../AppName/AppName";
+import Dropdown from "../../Components/Dropdown";
 
 const drawerWidth = 240;
 
@@ -30,6 +31,9 @@ export default function Sidebar(props: Props) {
     const { window } = props;
     const [mobileOpen, setMobileOpen] = React.useState(false);
     const [isClosing, setIsClosing] = React.useState(false);
+
+    // usePageフックを使用してユーザー情報を取得
+    const user = usePage().props.auth.user;
 
     const handleDrawerClose = () => {
         setIsClosing(true);
@@ -87,6 +91,45 @@ export default function Sidebar(props: Props) {
                     </ListItem>
                 ))}
             </List>
+            <Divider />
+            <Dropdown>
+                <Dropdown.Trigger>
+                    <span className="inline-flex rounded-md">
+                        <button
+                            type="button"
+                            className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                        >
+                            {user?.name}
+
+                            <svg
+                                className="-me-0.5 ms-2 h-4 w-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </button>
+                    </span>
+                </Dropdown.Trigger>
+
+                <Dropdown.Content>
+                    <Dropdown.Link href={route("profile.edit")}>
+                        Profile
+                    </Dropdown.Link>
+                    <Dropdown.Link
+                        href={route("logout")}
+                        method="post"
+                        as="button"
+                    >
+                        Log Out
+                    </Dropdown.Link>
+                </Dropdown.Content>
+            </Dropdown>
         </div>
     );
 
