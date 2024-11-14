61b5a6c386a61661eaeb1d80418e7960761e584b
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-11-14 11:24:05 +0000

    店舗追加ボタン実装

--- resources/js/Pages/Store/index.jsx
+++ resources/js/Pages/Store/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import StoreList from "./StoreList";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Button } from "@mui/material";
+import FormDialog from "@/Components/FormDialog";
 
 const Store = () => {
     return (
@@ -12,9 +13,46 @@ const Store = () => {
                 </h2>
             }
         >
-            <Button variant="contained" color="primary">
-                店舗登録
-            </Button>
+            <div className="mb-4">
+                <FormDialog
+                    className="mb-4"
+                    title="店舗情報|Store Information"
+                    buttonLabel="店舗登録 | Register Store"
+                    formList={[
+                        {
+                            id: "name",
+                            type: "text",
+                            label: "店舗名| Store Name",
+                            name: "name",
+                        },
+                        {
+                            id: "email",
+                            type: "email",
+                            label: "店舗のEメールアドレス|Stores Email Address",
+                            name: "email",
+                        },
+                        {
+                            id: "tel",
+                            type: "tel",
+                            label: "店舗の電話番号| Store Phone Number",
+                            name: "tel",
+                        },
+                        {
+                            id: "postal_code",
+                            type: "text",
+                            label: "店舗の郵便番号| Store Postal Code",
+                            name: "postal_code",
+                        },
+                        {
+                            id: "address",
+                            type: "text",
+                            label: "店舗の住所| Store Address",
+                            name: "address",
+                        },
+                    ]}
+                />
+            </div>
+
             <StoreList />
         </AuthenticatedLayout>
     );
