b92feaa7eee1dd0e1f6a6e6072f523fa8ea48fe7
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-11-20 00:43:56 +0000

    入力したデータをonSubmit関数に渡すまで完了

--- resources/js/Components/FormDialog.tsx
+++ resources/js/Components/FormDialog.tsx
@@ -6,6 +6,10 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import InputError from "./InputError";
 
 export type FormList = {
     required?: boolean;
@@ -23,9 +27,28 @@ type Props = {
     title: string;
     formList: FormList[];
     buttonLabel: string;
+    message: string;
 };
 
-export default function FormDialog({ buttonLabel, title, formList }: Props) {
+export default function FormDialog({
+    buttonLabel,
+    title,
+    formList,
+    message,
+}: Props) {
+    // Define the schema using zod
+    const formSchema = z.object({
+        name: z.string().min(1, "店舗名は必須です"),
+        email: z.string().email("正しいメールアドレスを入力してください"),
+        tel: z
+            .string()
+            .regex(/^\d+$/, "数字のみ入力してください")
+            .min(1, "電話番号は必須です"),
+        postal_code: z.string().min(1, "郵便番号は必須です"),
+        address: z.string().min(1, "住所は必須です"),
+    });
+    type FormData = z.infer<typeof formSchema>;
+
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -34,6 +57,19 @@ export default function FormDialog({ buttonLabel, title, formList }: Props) {
 
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(formSchema),
+        mode: "onChange",
+    });
+    const onSubmit = (data: any) => {
+        console.log(data, "submit data");
+        // handleClose();
     };
 
     return (
@@ -46,45 +82,55 @@ export default function FormDialog({ buttonLabel, title, formList }: Props) {
                 onClose={handleClose}
                 PaperProps={{
                     component: "form",
-                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-                        event.preventDefault();
-                        const formData = new FormData(event.currentTarget);
-                        const formJson = Object.fromEntries(
-                            (formData as any).entries()
-                        );
-                        const email = formJson.email;
-                        console.log(email);
-                        handleClose();
-                    },
+                    onSubmit: handleSubmit(onSubmit),
                 }}
             >
                 <DialogTitle>{title}</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        登録する店舗情報を入力してください
+                        {/* 登録する店舗情報を入力してください
                         <br />
-                        Please enter the store information to register
+                        Please enter the store information to register */}
+                        {message}
                     </DialogContentText>
                     <div className="px-3">
                         {formList.map((form) => (
-                            <TextField
-                                key={form.id}
-                                required={form.required ?? true}
-                                autoFocus={form.autoFocus ?? true}
-                                margin={form.margin ?? "dense"}
-                                id={form.name}
-                                name={form.name}
-                                label={form.label}
-                                type={form.type ?? "text"}
-                                fullWidth={form.fullWidth ?? true}
-                                variant={form.variant ?? "standard"}
-                            />
+                            <>
+                                <TextField
+                                    key={form.id}
+                                    // required={form.required ?? true}
+                                    autoFocus={form.autoFocus ?? true}
+                                    margin={form.margin ?? "dense"}
+                                    id={form.name}
+                                    name={form.name}
+                                    label={form.label}
+                                    type={form.type ?? "text"}
+                                    fullWidth={form.fullWidth ?? true}
+                                    variant={form.variant ?? "standard"}
+                                    {...register(
+                                        form.name as
+                                            | "name"
+                                            | "email"
+                                            | "tel"
+                                            | "postal_code"
+                                            | "address"
+                                    )}
+                                />
+                                <InputError
+                                    error={
+                                        errors[form.name as keyof typeof errors]
+                                    }
+                                    message={errors[form.name]?.message}
+                                />
+                            </>
                         ))}
                     </div>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button type="submit">Subscribe</Button>
+                    <Button onClick={handleClose}>キャンセル</Button>
+                    <Button variant="contained" type="submit" color="primary">
+                        登録
+                    </Button>
                 </DialogActions>
             </Dialog>
         </React.Fragment>
